8b6a438d317e8a6e9133947d2942411059c888b1
Andy Lazcano <36870910+Alazca@users.noreply.github.com>
2024-05-28 22:46:26 -0700

    Add files via upload
    updated polybar- battery and systry

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,3 +6,5 @@ killall -q polybar
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
 
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
